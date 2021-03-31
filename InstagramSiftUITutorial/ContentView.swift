f382903c9c07943f14f53444cb9add823f6ca379
Va4ko <ivanivanov291905@gmail.com>
2021-03-31 11:06:16 +0300

    Initial Commit

---
+++ InstagramSiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramSiftUITutorial
+//
+//  Created by Vachko on 31.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramSiftUITutorial/InstagramSiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSiftUITutorialApp.swift
+//  InstagramSiftUITutorial
+//
+//  Created by Vachko on 31.03.21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
